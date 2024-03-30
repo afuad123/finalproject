232ba944131fb42d097f407298a0b5f38e43cefe
afuad727 <afuad@guilford.edu>
2024-03-30 13:43:24 -0400

    first commit

---
+++ src/main/java/edu/guilford/MeatEater.java
@@ -0,0 +1,58 @@
+package edu.guilford;
+
+import java.util.Random;
+/**
+ * A class that represents a meat eater object in the simulation
+ * @Author A. Fuad
+ * @Version 2/29/24
+ * @See Critter
+ * @See PlantEater
+ */
+public class MeatEater extends Critter {
+
+    //ATTRIBUTES
+    /**
+     * An array of PlantEater objects that the meat eater object can chase and eat
+     */
+    private PlantEater[] plantEaters;
+    private Random rand = new Random();
+
+    //CONSTRUCTOR
+    /**
+     * A constructor that initializes a meat eater object with a size, growth rate, and amount of food needed; foodEaten will be set to zero
+     * @param size the initial size of the object (in g)
+     * @param rate  the initial rate of growth of the object (in g/day)
+     * @param foodNeed the amount of food the object needs each day (in g)
+     */
+    public MeatEater(float size, float rate, float foodNeed) {
+        super(size, rate, foodNeed);
+        
+    }
+
+    //METHODS
+    /**
+     * A method that simulates a day in the life of the meat eater; along with adding onto the simulateDay method in the Critter class,
+     * the meat eater object can chase and eat one or two plant eater objects a day, causing the plant eater to die.
+     */
+    @Override
+    public void simulateDay() {
+        //a meat eater object can chase one or two plant eater objects a day
+        int numChases = (rand.nextInt() * 2 + 1);
+        for (int i = 0; i < numChases; i++) {
+            //it has a specified probability of catching the plant eater object
+            if (rand.nextFloat() < 0.5) {
+                //if it does so, the meat eater will use its eat method with the current mass of the plant eater object
+                int plantToBeEaten = rand.nextInt(plantEaters.length);
+                eat(plantEaters[plantToBeEaten].getSize());
+                //the plant eater object will then invoke its die method
+                plantEaters[plantToBeEaten].die();
+            }
+            
+        }
+        super.simulateDay();
+    }
+
+
+
+}
+
---
+++ src/main/java/edu/guilford/Plant.java
@@ -0,0 +1,46 @@
+package edu.guilford;
+/**
+ * A class that represents a plant in the simulation
+ @author A. Fuad
+ @version 2/19/24
+ @see Creature
+ @see PlantEater
+ */
+public class Plant extends Creature {
+
+    //CONSTRUCTOR
+    /**
+     * A constructor that initializes a default of the Plant object with a size and growth rate
+     * @param size the initial size of the object (in g)
+     * @param rate the initial rate of growth of the object (in g/day)
+     */
+    public Plant(float size, float rate) {
+        super(size, rate);
+    }
+
+    //METHODS
+    /**
+     * The size of this object is decreased by a specified amount after being chewed on; the amount cannot exceed the current size of the object
+     * @param amount the amount by which to decrease the size (in g)
+     */
+    public void chewedOn(float amount) {
+        //if the amount is greater than the size, then the size is set to zero (entire plant object has been eaten)
+        if (amount > size) {
+            size = 0;
+            die();
+        } else {
+            size -= amount;
+        }
+    }
+    
+    /**
+     * A method that returns a string representation of the object
+     * @return a string representation of the object
+     */
+    @Override
+    public String toString() {
+        return "Plant: size = " + size + "g, rate = " + rate + "g/day, alive = " + alive + ", age = " + age + " days";
+    }
+
+}
+
