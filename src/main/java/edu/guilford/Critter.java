81aa4a7a59a240bcd34101adc69c2b121f577eda
afuad727 <afuad@guilford.edu>
2024-04-15 21:11:29 -0400

    rage rage against the dying of the light

--- src/main/java/edu/guilford/Critter.java
+++ src/main/java/edu/guilford/Critter.java
@@ -19,6 +19,20 @@ public abstract class Critter extends Creature {
      */
     protected float foodEaten;
 
+    /**
+     * A boolean that is true if the object can breathe and false if it cannot
+     * If the object cannot breathe, it will die 
+     */
+    protected boolean canBreathe;
+    /**
+     * A boolean that is true if the object can drink and false if it cannot
+     * If the object cannot drink (ie does not have access to clean water)
+     * its growth rate will begin decreasing by 1% each day
+     */
+    protected boolean canDrink;
+   
+
+
     //GETTERS AND SETTERS
     /**
      * A getter for foodNeed
@@ -35,16 +49,34 @@ public abstract class Critter extends Creature {
         this.foodNeed = foodNeed;
     }
 
+    /** 
+     * A setter for canBreathe
+     */
+    public void setCanBreathe(boolean canBreathe) {
+        this.canBreathe = canBreathe;
+    }
+
+    /**
+     * A setter for canDrink
+     */
+    public void setCanDrink(boolean canDrink) {
+        this.canDrink = canDrink;
+    }
+
     //CONSTRUCTOR
     /**
      * A constructor that initializes a default of the Critter object with a size, growth rate, and amount of food needed; foodEaten will be set to zero
      * @param size the initial size of the object (in g)
      * @param rate  the initial rate of growth of the object (in g/day)
      * @param foodNeed the amount of food the object needs each day (in g)
+     * @param canBreathe a boolean that is true if the object can breathe and false if it cannot
+     * @param canDrink a boolean that is true if the object can drink and false if it cannot
      */
-    public Critter(float size, float rate, float foodNeed) {
+    public Critter(float size, float rate, float foodNeed, boolean canBreathe, boolean canDrink) {
         super(size, rate);
         this.foodNeed = foodEaten = (15*size)/100; //needs to eat 15% of its size to survive
+        canBreathe = true;
+        canDrink = true;
     }
 
     //METHODS
@@ -56,7 +88,7 @@ public abstract class Critter extends Creature {
     @Override
     public void simulateDay() {
         super.simulateDay();
-        if (foodNeed > foodEaten) {
+        if (foodNeed > foodEaten || !canBreathe) {
             die();
         } else {
             foodEaten = 0;
--- src/main/java/edu/guilford/Human.java
+++ src/main/java/edu/guilford/Human.java
@@ -22,7 +22,7 @@ public class Human extends Critter {
     private MeatEater meatEaters[];
 
     public Human(float size, float rate, float foodNeed, Plant plants[], PlantEater plantEaters[], MeatEater meatEaters[]) {
-        super(size, rate, foodNeed);
+        super(size, rate, foodNeed, true, true);
         this.plants = plants;
         this.plantEaters = plantEaters;
         this.meatEaters = meatEaters;
@@ -35,14 +35,17 @@ public class Human extends Critter {
     @Override
     public void simulateDay() {
         eat();
+        drink();
+       //commute();
         super.simulateDay();
     }
 
     /**
      * A method that simulates the Human object eating; it chases/harvests a random number of food objects and eats them
+     * A human can chase 1-3 plantEaters a day and 1-2 meatEaters a day, with a 50% chance of catching each
+     * A human can also harvest 5 plants a day
      */
     public void eat() {
-        //a human can chase 1-3 plantEaters a day and 1-2 meatEaters a day, with a 50% chance of catching each
         int numPlants = (rand.nextInt() * 3 + 1);
         int numMeat = (rand.nextInt() * 2 + 1);
         for (int i = 0; i < numPlants; i++) {
@@ -59,12 +62,83 @@ public class Human extends Critter {
                 meatEaters[meatToBeEaten].die();
             }
         }
-        //a human can also harvest 5 plants a day
         for (int i = 0; i < 5; i++) {
             int plantToBeHarvested = rand.nextInt(plants.length);
             eat(plants[plantToBeHarvested].getSize());
             plants[plantToBeHarvested].die();
         }
+        polluteAir(size);
+    }
+
+    /**
+     * A method that simulates the Human object drinking
+     */
+    public void drink() {
+        polluteWater(size);
+    }
+
+    /**
+     * A method that simulates the Human object commuting, which results in 
+     * both air and water pollution based on the size of the vehicle
+     */
+    public void commute(String vehicle) {
+        if (vehicle == "walk") {
+            polluteAir(0);
+            polluteWater(0);
+        }
+        if (vehicle == "bike") {
+            polluteAir(0);
+            polluteWater(0);
+        }
+        if (vehicle == "car") {
+            float vehicleSize = size * 5;
+            polluteAir(vehicleSize);
+            polluteWater(vehicleSize/5);
+        }
+        if (vehicle == "bus") {
+            float vehicleSize = size * 10;
+            //human directly is not causing pollution but the bus still is
+            polluteAir(vehicleSize/10);
+            polluteWater(vehicleSize/10);
+        }
+    
+    }
+
+    //a method that makes the human pollute the air
+    public void polluteAir() {
+        //if the human pollutes the air by more than 90% of the air, the everything can't breathe
+        if (polluteAir(size) > 0.9) {
+            for (int i = 0; i < plantEaters.length; i++) {
+                plantEaters[i].setCanBreathe(false);
+            }
+            for (int i = 0; i < meatEaters.length; i++) {
+                meatEaters[i].setCanBreathe(false);
+            }
+        }
+
+    }
+
+    public float polluteAir(float amount) {
+        float pollutionAmount = amount*5/100;
+        return pollutionAmount;
+    }
+
+    //a method that makes the human pollute the water
+    public void polluteWater() {
+        //if the human pollutes the water by more than 50% of its size, the everything can't drink
+        if (polluteWater(size) > 0.9) {
+            for (int i = 0; i < plantEaters.length; i++) {
+                plantEaters[i].setCanDrink(false);
+            }
+            for (int i = 0; i < meatEaters.length; i++) {
+                meatEaters[i].setCanDrink(false);
+            }
+        }
+    }
+
+    public float polluteWater(float amount) {
+        float pollutionAmount = amount*5/100;
+        return pollutionAmount;
     }
 
     
